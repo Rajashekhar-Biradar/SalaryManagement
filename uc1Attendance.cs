dd28dec73014611c1a011c0b18c9a5ec64bb5770
Rajashekhar-Biradar <rajashekharbiradar41@gmail.com>
2022-10-06 22:45:16 +0530

    CheckAttendance

---
+++ uc1Attendance.cs
@@ -0,0 +1,19 @@
+﻿
+public class Employee
+{
+    public void CheckAttendance()
+    {
+        int IS_FULL_TIME = 1;
+        Random num = new Random();
+        int empCheck = num.Next(0, 2);
+
+        if (empCheck == IS_FULL_TIME)
+        {       
+            Console.WriteLine("Employee is present");
+        }
+        else
+        {
+            Console.WriteLine("Employee is absent");
+        }
+    }
+}
