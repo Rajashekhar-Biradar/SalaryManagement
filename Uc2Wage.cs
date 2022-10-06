b492a6219ef9e165d2aa0f8a95fff610f141a359
Rajashekhar-Biradar <rajashekharbiradar41@gmail.com>
2022-10-06 22:57:06 +0530

    EmpWage

---
+++ Uc2Wage.cs
@@ -0,0 +1,23 @@
+﻿public class employeeWage
+{
+    public void CheckEmpWage()
+    {
+        int IS_FULL_TIME = 1;
+        int Hr;
+        int empWage;
+        int RATE_PER_HR = 20;
+        Random num = new Random();
+        int empCheck = num.Next(0, 2);
+
+        if (empCheck == IS_FULL_TIME)
+        {
+            Hr = 8;
+        }
+        else
+        {
+            Hr = 0;
+        }
+        empWage = Hr * RATE_PER_HR;
+        Console.WriteLine("Employee wage is UC2: "+empWage);
+    }
+}
